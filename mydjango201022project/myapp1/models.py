460632d739653a31580a21ca57870e2411663d10
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-10-20 22:12:44 +0300

    models.py

--- mydjango201022project/myapp1/models.py
+++ mydjango201022project/myapp1/models.py
@@ -6,3 +6,7 @@ class Worker(models.Model):
     last_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f'{self.last_name} {self.name}: {self.salary} руб.'
+
+
