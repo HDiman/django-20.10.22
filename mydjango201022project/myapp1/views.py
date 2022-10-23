ecc7776e9a3d91dd5b2ee7e1afdfe2c476861d5d
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-10-23 13:22:18 +0300

    views.py redacting

--- mydjango201022project/myapp1/views.py
+++ mydjango201022project/myapp1/views.py
@@ -6,8 +6,12 @@ def index_page(request):
     all_workers = Worker.objects.all()
     print(all_workers)
 
+    # Filtering some data
     workers_filter = Worker.objects.filter(salary=100000)
     print(workers_filter)
+
+    for i in all_workers:
+        print(f'Имя: {i.name}, Фамилия: {i.last_name}, Зарплата: {i.salary}, Поряд.номер: {i.id}')
 
     return render(request, 'index.html')
 
