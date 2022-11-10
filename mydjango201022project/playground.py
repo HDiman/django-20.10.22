7777a4f88a57b2f73fe05cf63d29dfbb9e24b142
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-11-10 22:02:39 +0300

    Plan for a game

--- mydjango201022project/playground.py
+++ mydjango201022project/playground.py
@@ -9,3 +9,10 @@
 print('1' in '123')
 
 # Here is a plan for a new game
+
+# 1. Set Django on Server
+# 2. Set squeries
+# 3. Set points
+# 4. Play with others players
+# 5. Teach Computer
+# 6. Play with Computer
